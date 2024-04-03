612a3d9cd471a33a1f816010ba323f876225d530
Paulinka26 <268403@student.pwr.edu.pl>
2024-04-04 01:24:54 +0200

    nowe

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,12 +1,22 @@
-import android.R
+package com.example.project
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_item)
+        setContentView(R.layout.activity_main)
+    }
 
-        // Tutaj możesz umieścić swoje własne kodowanie
+    fun loginButtonClicked(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
+
+class LoginActivity {
+
+}
+
